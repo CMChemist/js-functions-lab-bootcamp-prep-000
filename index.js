db2099123788eea31cfa10e81162662b738f4ffc
global-value-0259 <josephtrettevik@gmail.com>
2019-05-26 18:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (name) {
+  var string = 'Happy holidays, '+ ${name} + '!';
+  return string
+}
+
+function holidayCountdown (holiday, days) {
+  return 'It\'s ' + ${days} + ' days until ' + ${holiday} + '!';
+}
